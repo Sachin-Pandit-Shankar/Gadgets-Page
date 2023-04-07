6abe1bcd1497bf3849d6bb5fb07bbff9b57f85e8
Sachin pandit <127498624+Sachin-Pandit-Shankar@users.noreply.github.com>
2023-04-07 18:04:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/gadgets", (request, response) => {
-  Response.senFile("../gadgets.html", { root: __dirname });
+  response.senFile("../gadgets.html", { root: __dirname });
 });
 module.exports = app;
